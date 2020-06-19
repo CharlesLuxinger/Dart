ff85a8f4448991586c47481c209a3ddbe28ec31b
Charles Luxinger <charles.luxinger@picpay.com>
2020-06-19 20:48:46 -0300

    Variables

---
+++ HelloWorld/bin/Variables.dart
@@ -0,0 +1,12 @@
+void main() {
+  int age = 25;
+  double heigth = 1.85;
+  String name = 'Charles';
+
+  var job = 'Developer';
+
+  dynamic salary = 5000.0;
+  salary = 'none';
+
+  print('My name is $name, I\'m $age, $heigth meters and I\'m $job, salary is $salary');
+}
