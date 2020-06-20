1b68952eee83edf8fe6f3e4efea12c24c97f9081
Charles Luxinger <charles.luxinger@picpay.com>
2020-06-19 22:29:15 -0300

    Métodos

--- ByteBank/bin/ByteBank.dart
+++ ByteBank/bin/ByteBank.dart
@@ -11,6 +11,9 @@ void main() {
   print('Conta: ${conta.conta}');
   print('Saldo: ${conta.saldo}');
 
+  conta.saque(10);
+  var saldo = conta.deposito(100);
+  print(saldo);
 }
 
 class ContaCorrente {
@@ -18,4 +21,22 @@ class ContaCorrente {
   int agencia;
   int conta;
   double saldo = 0;
+
+  void saque(double valor){
+    saldo -= valor;
+  }
+
+  double deposito(double valor) {
+    return saldo += valor;
+  }
+
+  bool transferencia(double valor, ContaCorrente contaDestino){
+    if(saldo < valor){
+      return false;
+    } else {
+      saldo -= valor;
+      contaDestino.saldo += valor;
+      return true;
+    }
+  }
 }
